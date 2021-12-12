26993d7f3ba5f4e17cb1864e94192e37d1fc7ddd
QUERY-GANI <ahmadxgani01@gmail.com>
2021-12-12 08:06:02 +0700

    add php file for list the directory

---
+++ index.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>List latihan frontend</title>
+  </head>
+  <body>
+    <a href="./latihan-box-model">box model</a>
+    <a href="./latihan-flexbox">flexbox</a>
+    <a href="./latihan-float">float</a>
+    <a href="./portfolio_vanilla_css">portfolio</a>
+    <a href="./uji-coba">uji coba</a>
+  </body>
+</html>
